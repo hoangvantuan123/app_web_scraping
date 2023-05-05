22c490d0661276ed73814c3b050f9a155c6ca931
hoangvantuan123 <rammatuan@gmail.com>
2023-05-05 11:11:58 +0700

    Update code

---
+++ README.md
@@ -0,0 +1,31 @@
+
+
+# Screen Scraping: Tổng quan về kỹ thuật và cách thực hiện
+
+Screen scraping là một kỹ thuật được sử dụng để thu thập thông tin từ các trang web hoặc ứng dụng web khác mà không cần phải sử dụng các API hoặc giao diện lập trình ứng dụng (APIs). Kỹ thuật này giúp cho các nhà phát triển có thể thu thập thông tin từ các trang web hoặc ứng dụng web khác để phân tích dữ liệu hoặc tìm kiếm thông tin.
+
+Bạn có thể xem một vài đoạn code mẫu về kỹ thuật Screen Scraping trong [web_scraping](web_scraping/)
+
+## Các công cụ Screen Scraping phổ biến
+
+Có nhiều công cụ Screen Scraping phổ biến có sẵn để giúp cho việc thu thập dữ liệu từ các trang web hoặc ứng dụng web trở nên dễ dàng hơn. Dưới đây là một số công cụ phổ biến:
+
+1. Beautiful Soup: Là một thư viện Python phổ biến được sử dụng để phân tích HTML và XML. Nó cung cấp cho người dùng các phương thức để lấy dữ liệu từ các trang web một cách nhanh chóng và dễ dàng.
+
+2. Scrapy: Là một framework Python phổ biến cho phép người dùng thu thập dữ liệu từ các trang web một cách tự động. Scrapy có thể sử dụng để lấy dữ liệu từ một hoặc nhiều trang web và tổ chức chúng vào một cách hiệu quả.
+
+3. Selenium: Là một công cụ phổ biến cho phép người dùng tự động hóa các hành động trên trình duyệt web. Nó cung cấp cho người dùng các phương thức để tương tác với các trang web một cách tự động và thu thập dữ liệu từ đó.
+
+## Những lưu ý khi sử dụng Screen Scraping
+
+Khi sử dụng kỹ thuật Screen Scraping, bạn cần lưu ý những điều sau:
+
+1. Luôn luôn tuân thủ các quy định và điều khoản sử dụng của các trang web. Việc lấy dữ liệu từ các trang web mà không được phép có thể dẫn đến hậu quả pháp lý.
+
+2. Cẩn thận khi sử dụng các công cụ Screen Scraping. Việc sử dụng các công cụ không đúng cách có thể gây ra tình trạng spam hoặc tình trạng server overload cho các trang web, gây ảnh hưởng đến trải nghiệm của người dùng.
+
+3. Luôn cập nhật và kiểm tra lại các mã code của mình. Việc sử dụng các công cụ Screen Scraping có thể bị phát hiện và chặn bởi các trang web, do đó, cần thường xuyên cập nhật và kiểm tra lại mã code để đảm bảo sự ổn định và hiệu quả.
+
+## [Một chương trình về Screen Scraping](https://hoangvantuan123-app-web-scraping-streamlit-app-76giua.streamlit.app/) 
+![](img/box_1.png) 
+
--- web_scraping/weather_city_all.py
+++ web_scraping/weather_city_all.py
@@ -4,98 +4,96 @@ from geopy.geocoders import Nominatim
 from bs4 import BeautifulSoup as soup
 import csv
 
-# Lu file duoi dang csv
-filename = "weather_city_data.csv"
-f = open(filename, "w")
-
-headers = "city,humidity,wind_speed,barometer,dewpoint,visibility,last_update\n"
-
-f.write(headers)
 
 with open('uscities_v2.csv', 'r') as f:
     reader = csv.reader(f)
     next(reader)  # skip header
 
-    for row in reader:
-        city, state_code = row[0], row[1]
-        # Tìm toạ độ của thành phố
-        try:
-            # khởi tạo một đối tượng Nominatim
-            geolocator = Nominatim(user_agent="weather_app")
-            # Tìm kiếm toạ độ theo địa chỉ nhập
-            location = geolocator.geocode(f"{city}, {state_code}")
-            # print ra toạ độ kinh độ , vĩ đỗ của địa chỉ vừa mới nhập.
-            latitude, longitude = location.latitude, location.longitude
-        except AttributeError:
-            print(
-                'Không tìm thấy địa điểm thành phố. Vui lòng kiểm tra lại tên thành phố và mã bang đã nhập!')
+       # Lu file duoi dang csv
+    filename = "weather_city_data.csv"
+    with open(filename, "w") as f_out:
+        headers = '"city","humidity","wind_speed","barometer","dewpoint","visibility","last_update"\n'
+        f_out.write(headers)
 
-        # Tạo URL mới với toạ độ của thành phố
-        # .4f được sử dụng để định dạng các giá trị số thập phân của latitude và longitude với độ chính xác đến 4 chữ số sau dấu phẩy.
-        url = f"https://forecast.weather.gov/MapClick.php?lat={latitude:.4f}&lon={longitude:.4f}"
-        response = urllib.request.urlopen(url)
-        content = response.read()
+        for row in reader:
+            city, state_code = row[0], row[1]
+            # Tìm toạ độ của thành phố
+            try:
+                # khởi tạo một đối tượng Nominatim
+                geolocator = Nominatim(user_agent="weather_app")
+                # Tìm kiếm toạ độ theo địa chỉ nhập
+                location = geolocator.geocode(f"{city}, {state_code}")
+                # print ra toạ độ kinh độ , vĩ đỗ của địa chỉ vừa mới nhập.
+                latitude, longitude = location.latitude, location.longitude
+            except AttributeError:
+                print(
+                    'Không tìm thấy địa điểm thành phố. Vui lòng kiểm tra lại tên thành phố và mã bang đã nhập!')
 
-        # Ghi sang 1 file
-        # open('weather_file_page.html', 'wb').write(content)
-        # print(content)
+            # Tạo URL mới với toạ độ của thành phố
+            # .4f được sử dụng để định dạng các giá trị số thập phân của latitude và longitude với độ chính xác đến 4 chữ số sau dấu phẩy.
+            url = f"https://forecast.weather.gov/MapClick.php?lat={latitude:.4f}&lon={longitude:.4f}"
+            response = urllib.request.urlopen(url)
+            content = response.read()
 
-        # Solution #2
-        page_soup = soup(content, 'html.parser')
-        try:
+            # Ghi sang 1 file
+            # open('weather_file_page.html', 'wb').write(content)
+            # print(content)
 
-            div_page_city = page_soup.find('div', {'id': 'seven-day-forecast'})
-            div_page_city_div = div_page_city.find(
-                'div', {'class': 'panel-heading'})
-            h2_page = div_page_city_div.find(
-                'h2', {'class': 'panel-title'}).text.strip()
-            print(h2_page)
-        except:
-            h2_page = 'N/A'
-        # dùng find_next_sibling() đây là một phương thức được sử dụng để tìm phần tử kế tiếp trong danh sách
-        try:
-            humidity = page_soup.find(
-                'td', text='Humidity').find_next_sibling('td').text
-        except AttributeError:
-            humidity = 'N/A'
+            # Solution #2
+            page_soup = soup(content, 'html.parser')
+            try:
 
-        try:
-            wind_speed = page_soup.find(
-                'td', text='Wind Speed').find_next_sibling('td').text
-        except AttributeError:
-            wind_speed = 'N/A'
+                div_page_city = page_soup.find('div', {'id': 'seven-day-forecast'})
+                div_page_city_div = div_page_city.find(
+                    'div', {'class': 'panel-heading'})
+                h2_page = div_page_city_div.find(
+                    'h2', {'class': 'panel-title'}).text.strip()
+                #print(h2_page)
+            except:
+                h2_page = 'N/A'
+            # dùng find_next_sibling() đây là một phương thức được sử dụng để tìm phần tử kế tiếp trong danh sách
+            try:
+                humidity = page_soup.find(
+                    'td', text='Humidity').find_next_sibling('td').text
+            except AttributeError:
+                humidity = 'N/A'
 
-        try:
-            barometer = page_soup.find(
-                'td', text='Barometer').find_next_sibling('td').text
-        except AttributeError:
-            barometer = 'N/A'
+            try:
+                wind_speed = page_soup.find(
+                    'td', text='Wind Speed').find_next_sibling('td').text
+            except AttributeError:
+                wind_speed = 'N/A'
 
-        try:
-            dewpoint = page_soup.find(
-                'td', text='Dewpoint').find_next_sibling('td').text
-        except AttributeError:
-            dewpoint = 'N/A'
+            try:
+                barometer = page_soup.find(
+                    'td', text='Barometer').find_next_sibling('td').text
+            except AttributeError:
+                barometer = 'N/A'
 
-        try:
-            visibility = page_soup.find(
-                'td', text='Visibility').find_next_sibling('td').text
-        except AttributeError:
-            visibility = 'N/A'
+            try:
+                dewpoint = page_soup.find(
+                    'td', text='Dewpoint').find_next_sibling('td').text
+            except AttributeError:
+                dewpoint = 'N/A'
 
-        try:
-            last_update = page_soup.find(
-                'td', text='Last update').find_next_sibling('td').text.strip()
-        except AttributeError:
-            last_update = 'N/A'
+            try:
+                visibility = page_soup.find(
+                    'td', text='Visibility').find_next_sibling('td').text
+            except AttributeError:
+                visibility = 'N/A'
 
-        print('Humidity:', humidity)
-        print('Wind Speed:', wind_speed)
-        print('Barometer:', barometer)
-        print('Dewpoint:', dewpoint)
-        print('Visibility:', visibility)
-        print('Last update:', last_update)
+            try:
+                last_update = page_soup.find(
+                    'td', text='Last update').find_next_sibling('td').text.strip()
+            except AttributeError:
+                last_update = 'N/A'
 
-        f.write(
-            f"{h2_page},{humidity},{wind_speed},{barometer}, {dewpoint} ,{visibility}, {last_update}\n")
-    f.close()
+            print('Humidity:', humidity)
+            print('Wind Speed:', wind_speed)
+            print('Barometer:', barometer)
+            print('Dewpoint:', dewpoint)
+            print('Visibility:', visibility)
+            print('Last update:', last_update)
+            #f_out.write(f"{h2_page},{humidity},{wind_speed},{barometer},{dewpoint},{visibility},{last_update}\n")
+            f_out.write(f"\"{h2_page}\",\"{humidity}\",\"{wind_speed}\",\"{barometer}\",\"{dewpoint}\",\"{visibility}\",\"{last_update}\"\n")
+        f_out.close()
